added multiple activation functions; fixed degree calculation

dna.py
@@ -25,6 +25,11 @@ class DNA():
             self.weightMatrixHiddenLayerOne = childMatrixOne
             self.weightMatrixOutput = childMatrixTwo
 
+        # Vectorize functions for later use
+        self.vectorizedSigmoid = np.vectorize(snakeUtil.sigmmoid)
+        self.vectorizedRectifier = np.vectorize(snakeUtil.rectified)
+        self.vectorizedTanH = np.vectorize(snakeUtil.tanH)
+
     def fitness(self, score, steps):
         self.fitnessValue = 2**score / steps
 
@@ -55,13 +60,18 @@ class DNA():
         inputVector.append(snakeUtil.calcDegree(snakeHeadMidX, snakeHeadMidY,
                                                 foodXMid, foodYMid, direction))
         # Multiply the weight matrix with the input vector
-        valuesHiddenLayer = np.dot(inputVector, self.weightMatrixHiddenLayerOne)
+        valuesHiddenLayer = np.dot(
+            inputVector, self.weightMatrixHiddenLayerOne)
+        # Apply sigmoid
+        self.vectorizedSigmoid(valuesHiddenLayer)
         valuesOutput = np.dot(valuesHiddenLayer, self.weightMatrixOutput)
+        # Apply sigmoid
+        self.vectorizedSigmoid(valuesOutput)
         print("input: \n", inputVector)
-        print("weight: \n", self.weightMatrixHiddenLayerOne)
-        print("weight x input: \n", valuesHiddenLayer)
-        print("weightMatrixOutput: \n", self.weightMatrixOutput)
-        print("VALUESOUTPUT: \n", valuesOutput)
+        # print("weight: \n", self.weightMatrixHiddenLayerOne)
+        # print("weight x input: \n", valuesHiddenLayer)
+        # print("weightMatrixOutput: \n", self.weightMatrixOutput)
+        # print("VALUESOUTPUT: \n", valuesOutput)
 
         # Check which value is the biggest;
         # indexposition decides which action to take: 0 -> left; 1 -> right; 2 -> forward
@@ -86,3 +96,8 @@ class DNA():
         hiddenOneToOutputWeights = (np.random.rand(neuronCountHiddenLayerOne,
                                                    outputNeurons) - 0.5) * 2
         return inputToHiddenOneWeights, hiddenOneToOutputWeights
+
+    # Taking a list of numbers which stand for the numbers of neurons for the specific layer.
+    # First and last layers are always input and output layers respectively.
+    def createNN(self, list):
+        pass

snakeUtil.py
@@ -3,13 +3,14 @@ import numpy as np
 # Calculates the angle from the head to the food and
 # which is then represented from -1 to 1 (-1 left, 0 straight forward, 1 right)
 
+
 def calcDegree(snakeX, snakeY, foodX, foodY, direction):
     calc = math.atan2(snakeY - foodY, snakeX - foodX)
     switch = {
         "Up": -math.pi / 2,
         "Down": math.pi / 2,
         "Left": 0,
-        "Right": math.pi
+        "Right": -math.pi
     }
     return math.sin(calc + switch.get(direction))
 
@@ -103,5 +104,14 @@ def convertDirectionRelativeToSnake(direction, newDirection):
         result = "Forward"
     return result
 
+
 def sigmmoid(x):
     return 1/(1 + np.exp(-x))
+
+
+def rectified(x):
+    return max(0, x)
+
+
+def tanH(x):
+    return math.tanh(x)